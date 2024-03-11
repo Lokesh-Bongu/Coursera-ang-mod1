b8615bec438ba9ffc7e15348ded217b5c4ce4018
lokesh-Bongu <lokesh.bongu@tpsouthernodisha.com>
2024-03-11 11:37:12 +0530

    Initial2 webFile

--- app.js
+++ app.js
@@ -28,8 +28,8 @@
           $scope.textBoxBorderColor = 'green';
         } else {
           $scope.message = 'Too much!';
-          $scope.messageColor = 'green'; // Change to red color for "Too much!"
-          $scope.textBoxBorderColor = 'green'; // Change to red border color for "Too much!"
+          $scope.messageColor = 'green'; // Change to green color for "Too much!"
+          $scope.textBoxBorderColor = 'green'; // Change to green border color for "Too much!"
         }
       };
     }
